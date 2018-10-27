030ad2d40fa86483d7198f7ef85e7b67e1523eaa
victor <victormolina1@stu.bmcc.cuny.edu>
2018-10-27 14:06:07 -0400

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+  const IMAGE_WIDTH = 500;
+  const IMAGE_HEIGHT = 342;
+
+  function setup() {
+      createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
+  }
+
+  function draw() {
+      stroke(198,157,101);
+      line(292, 1, 465, 261);
+  }
